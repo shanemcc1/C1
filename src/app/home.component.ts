d63e0cfa71e3fef6bd7f69e6c9da9fe7180dcf07
Shane McCaughan <mccaughan-s3@ulster.ac.uk>
2020-12-08 02:24:10 +0000

    home

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { WebService } from './web.service';
 import { AuthService} from './auth.service';
 import { EpisodesComponent} from './episodes.component';
+import { EpisodeComponent} from './episode.component';
 
 @Component({
   selector: 'home',
@@ -9,5 +10,13 @@ import { EpisodesComponent} from './episodes.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, public webService: WebService) {
+  }
+  season = 1;
+  season_page_size = 8;
+
+  ngOnInit() {
+
+    this.webService.getSeasons(this.season, this.season_page_size);
+  }
 }
